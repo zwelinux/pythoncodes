0b5f4d3ae8863c65de258016bf56bbed2b6e9779
zwelinuxfreelance <zwelinux@gmail.com>
2024-04-29 09:22:28 +0630

    test18

---
+++ test18.py
@@ -0,0 +1,13 @@
+myfav_artists = ['chris brown', 'khine maung toe', 'fleetwood mac', 'khin maung htoo', 'khin maung zaw']
+
+print(type(myfav_artists))
+
+print(myfav_artists[2])
+print(myfav_artists[1:4])
+print(myfav_artists[1:])
+print(myfav_artists[:4])
+
+
+print(myfav_artists)
+print(myfav_artists[0]) # index 
+print(len(myfav_artists))
